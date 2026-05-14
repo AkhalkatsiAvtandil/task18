c4ebf0eda0af1f12c2e9f586132e4a7802a1f078
AkhalkatsiAvtandil <akhalkatsiavto@gmail.com>
2026-05-15 02:44:07 +0400

    task1

---
+++ js/app.js
@@ -0,0 +1,22 @@
+import { data } from "./data.js";
+// console.log(data);
+// task1
+
+// let words = [
+//   {
+//     a: String,
+//   },
+//   {
+//     b: "",
+//   },
+// ];
+let a = "string";
+let b = "";
+// let "" = 0;
+
+function getemptyornotempty(a = true, b = false) {
+  //   let a = "string";
+  //   let b = "";
+  console.log(a);
+}
+getemptyornotempty();
